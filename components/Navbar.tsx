94173d69a791d1ad372ec8483a344262783bee19
Carbon <robotcreeper13@gmail.com>
2021-03-11 21:42:32 -0600

    Fix z-index for navbar

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -6,7 +6,7 @@ export default function Navbar({ activePage="" }: {
 }): JSX.Element {
   let [MenuOpen, setMenuOpen] = useState(false);
   return (
-    <nav className="bg-gray-800 p-2 w-full fixed">
+    <nav className="bg-gray-800 p-2 w-full fixed z-50">
       <div className="max-w-7xl mx-auto px-2 sm:px-6 lg:px-8">
         <div className="relative flex items-center justify-between h-16">
           <div className="absolute inset-y-0 left-0 flex items-center sm:hidden">
