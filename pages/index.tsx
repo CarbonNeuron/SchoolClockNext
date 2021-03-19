34feacab346d2abbda3c79d4e351ba2b6ff5e7e8
Carbon <robotcreeper13@gmail.com>
2021-03-19 07:36:18 -0500

    Change index and MS back to normal sound

--- pages/index.tsx
+++ pages/index.tsx
@@ -110,7 +110,7 @@ export default function Home() {
             },
             "retina_detect": true
           }} className="bg-gray-900 h-screen w-screen"/>
-        <SchoolClock PeriodTimes={ClassSchedule} PeriodHeadings={PeriodHeadings} PeriodNames={ClassNames} timeZone={"America/Chicago"} soundPath="popcat.wav"/>
+        <SchoolClock PeriodTimes={ClassSchedule} PeriodHeadings={PeriodHeadings} PeriodNames={ClassNames} timeZone={"America/Chicago"} soundPath="tone2.ulaw.wav"/>
 
     </div>
   );
--- pages/ms.tsx
+++ pages/ms.tsx
@@ -102,7 +102,7 @@ export default function ms() {
           },
           "retina_detect": true
         }} className="bg-gray-900 h-screen w-screen"/>
-      <SchoolClock PeriodTimes={ClassSchedule} PeriodHeadings={PeriodHeadings} PeriodNames={ClassNames} timeZone={"America/Chicago"} soundPath="popcat.wav"/>
+      <SchoolClock PeriodTimes={ClassSchedule} PeriodHeadings={PeriodHeadings} PeriodNames={ClassNames} timeZone={"America/Chicago"} soundPath="tone2.ulaw.wav"/>
 
     </div>
   );
